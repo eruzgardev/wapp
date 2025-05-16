3e1c1cb72a0fe542d284a3755ef6eddb80c4892d
eruzgardev <eruzgardev@gmail.com>
2025-05-16 14:07:02 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -255,7 +255,6 @@ export default function Home() {
                           key={field.name}
                           onClick={() =>
                             setSortFields((sortFields) => ({
-                              ...sortFields,
                               [field.name]: (sortFields[field.name] ?? 1) * -1,
                             }))
                           }
